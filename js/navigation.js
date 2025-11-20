01a8b4e5d8097517507189c4840f3d0f3b86238b
Rhythm Desai <rhythmdesai22@gmail.com>
2025-11-20 17:46:22 -0500

    Remove/move service worker registration file

--- js/navigation.js
+++ js/navigation.js
@@ -225,3 +225,16 @@ document.addEventListener('headerLoaded', function () {
     updateNavigationLinks();
   }, 100);
 });
+
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function (registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(function (err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
